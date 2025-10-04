45a2b521332239e2952efd4d72d0e6ce759129ae
Eduardo Henrique <eduardo.hnsantos@gmail.com>
2025-10-04 18:48:47 -0300

    update

--- dags/api_to_postgres.py
+++ dags/api_to_postgres.py
@@ -1,38 +1,53 @@
-
 from airflow.decorators import task, dag
-
-from airflow.decorators import gerar_numero_aleatorio, fetch_pokemon_data, add_pokemon_to_db
-
 from datetime import datetime
+import requests, random, psycopg2
 
-@dag(dag_id="api_postgres",
-     description="pipeline_para_capturar_pokemon",
-     start_date=datetime(2025, 10, 5),
-     schedule="* * * * *",
-     catchup=False)
+@dag(
+    dag_id="api_postgres",
+    description="pipeline_para_capturar_pokemon",
+    start_date=datetime(2025, 10, 5),
+    schedule="* * * * *",
+    catchup=False
+)
 def api_postgres():
 
     @task(task_id="gerar_numero_aleatorio")
-    def task_gerar_numero_aleatorio():
-        return gerar_numero_aleatorio()
+    def gerar_numero_aleatorio():
+        return random.randint(1, 151)
 
     @task(task_id="fetch_pokemon_data")
-    def task_fetch_pokemon_data():
-        return fetch_pokemon_data()
-    
+    def fetch_pokemon_data(pokemon_id: int):
+        url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}"
+        response = requests.get(url)
+        return response.json()
+
     @task(task_id="add_pokemon_to_db")
-    def task_add_pokemon_to_db(pokemon_data):
-        add_pokemon_to_db(pokemon_data)    
+    def add_pokemon_to_db(pokemon_data: dict):
+        conn = psycopg2.connect(
+            host="seu_host",
+            dbname="seu_db",
+            user="seu_usuario",
+            password="sua_senha"
+        )
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO pokemons (id, name, base_experience) VALUES (%s, %s, %s)",
+            (pokemon_data["id"], pokemon_data["name"], pokemon_data["base_experience"])
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+        return f"Pokemon {pokemon_data['name']} inserido com sucesso!"
 
-    @task
+    @task(task_id="print_de_sucesso")
     def print_de_sucesso(response):
-        print(response)    
-    
+        print(response)
 
-    t1 = task_gerar_numero_aleatorio()
-    t2 = task_fetch_pokemon_data(t1)
-    t3 = task_add_pokemon_to_db(t2)
-    t4 = print_de_sucesso(t3)
+    # Definindo o fluxo
+    pokemon_id = gerar_numero_aleatorio()
+    pokemon_data = fetch_pokemon_data(pokemon_id)
+    insert_response = add_pokemon_to_db(pokemon_data)
+    print_de_sucesso(insert_response)
 
-    t1 >> t2 >> t3 
-api_postgres()    
+
+api_postgres()
